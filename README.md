a6cdbdd752ca2940ba846b8fd17c26d8ce9135c5
Miguel Rentes <miguel.rentes@gmail.com>
2017-04-21 16:44:12 +0100

    Refactors scripts.

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ A simple docker-based OpenLDAP client server tutorial
 - OpenLDAP is based on docker image from [osixia/openldap:latest](https://hub.docker.com/r/osixia/openldap/).
 - phpLDAPadmin is bases on docker image from [osixia/phpldapadmin:latest](https://hub.docker.com/r/osixia/phpldapadmin/).
 
-Usage: server.sh [start|stop].
+Usage: server.sh [start | stop].
 
 The start option runs the OpenLDAP + phpLDAPadmin docker containers.
 The stop options stops the containers and removes them.
@@ -19,7 +19,7 @@ The stop options stops the containers and removes them.
 
 - OpenLDAP is based on docker image from [osixia/openldap:latest](https://hub.docker.com/r/osixia/openldap/).
 
-Usage: client.sh [start|stop].
+Usage: client.sh [start | stop].
 
 The start option runs the OpenLDAP container and performs an ldap search from the command-line.
 The stop options stops the container and removes them.
--- server.sh
+++ server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 startcontainer() {
-    docker run --name ldap-server --hostname ldap-server --detach osixia/openldap
-    docker run --name phpldapadmin-service --hostname phpldapadmin-service --link ldap-server:ldap-host --env PHPLDAPADMIN_LDAP_HOSTS=ldap-host -p 443:443 --detach osixia/phpldapadmin
+    docker run --name ldap-service --hostname ldap-service --detach osixia/openldap
+    docker run --name phpldapadmin-service --hostname phpldapadmin-service --link ldap-service:ldap-host --env PHPLDAPADMIN_LDAP_HOSTS=ldap-host -p 443:443 --detach osixia/phpldapadmin
 
     echo "Go to: https://localhost/"
     echo "Login DN: cn=admin,dc=example,dc=org"
@@ -10,9 +10,9 @@ startcontainer() {
 }
 
 stopcontainer() {
-    docker stop ldap-server
+    docker stop ldap-service
     docker stop phpldapadmin-service
-    docker rm ldap-server
+    docker rm ldap-service
     docker rm phpldapadmin-service
 }
 
