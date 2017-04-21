cc25f49814c277dc91a7dd2ddd550a806504b702
Miguel Rentes <miguel.rentes@gmail.com>
2017-04-21 15:21:58 +0100

    Refactors client bash script.

--- client.sh
+++ client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
-docker run --name ldap-client --hostname ldap-client --detach osixia/openldap
-docker exec -it ldap-client ldapsearch -D "cn=admin,dc=example,dc=org" -h localhost -w admin -b "dc=example,dc=org" -s sub "(objectclass=*)"
+docker run --name ldap-client --hostname ldap-client --link ldap-service:ldap-service --detach osixia/openldap
+docker exec -it ldap-client ldapsearch -D "cn=admin,dc=example,dc=org" -h ldap-service -w admin -b "dc=example,dc=org" -s sub "(objectclass=*)"
