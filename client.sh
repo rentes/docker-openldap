d37973b0acd97be3550465284b871f59f9a38582
Miguel Rentes <miguel.rentes@gmail.com>
2017-04-21 22:48:49 +0100

    aRefactors client bash script

--- client.sh
+++ client.sh
@@ -2,6 +2,7 @@
 
 startcontainer() {
     docker run --name ldap-client --hostname ldap-client --link ldap-service:ldap-service --detach osixia/openldap
+    echo "Performing LDAP search"
     docker exec -it ldap-client ldapsearch -D "cn=admin,dc=example,dc=org" -h ldap-service -w admin -b "dc=example,dc=org" -s sub "(objectclass=*)"
 }
 
