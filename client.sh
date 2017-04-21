642723743e0b3028310968b95bedfde9067101be
Miguel Rentes <miguel.rentes@gmail.com>
2017-04-21 16:19:38 +0100

    Refactors client and server bash scripts

--- client.sh
+++ client.sh
@@ -1,3 +1,20 @@
 #!/bin/bash -e
-docker run --name ldap-client --hostname ldap-client --link ldap-service:ldap-service --detach osixia/openldap
-docker exec -it ldap-client ldapsearch -D "cn=admin,dc=example,dc=org" -h ldap-service -w admin -b "dc=example,dc=org" -s sub "(objectclass=*)"
+
+startcontainer() {
+    docker run --name ldap-client --hostname ldap-client --link ldap-service:ldap-service --detach osixia/openldap
+    docker exec -it ldap-client ldapsearch -D "cn=admin,dc=example,dc=org" -h ldap-service -w admin -b "dc=example,dc=org" -s sub "(objectclass=*)"
+}
+
+stopcontainer() {
+    docker stop ldap-client
+    docker rm ldap-client
+}
+
+case "$1" in 
+    start)   startcontainer ;;
+    stop)    stopcontainer ;;
+    restart) stopcontainer; startcontainer ;;
+    *) echo "usage: $0 start|stop|restart" >&2
+       exit 1
+       ;;
+esac
--- server.sh
+++ server.sh
@@ -1,8 +1,26 @@
 #!/bin/bash -e
-docker run --name ldap-service --hostname ldap-service --detach osixia/openldap
-docker run --name phpldapadmin-service --hostname phpldapadmin-service --link ldap-service:ldap-host --env PHPLDAPADMIN_LDAP_HOSTS=ldap-host -p 443:443 --detach osixia/phpldapadmin
 
-echo "Go to: https://localhost/"
-echo "Login DN: cn=admin,dc=example,dc=org"
-echo "Password: admin"
+startcontainer() {
+    docker run --name ldap-server --hostname ldap-server --detach osixia/openldap
+    docker run --name phpldapadmin-service --hostname phpldapadmin-service --link ldap-server:ldap-host --env PHPLDAPADMIN_LDAP_HOSTS=ldap-host -p 443:443 --detach osixia/phpldapadmin
 
+    echo "Go to: https://localhost/"
+    echo "Login DN: cn=admin,dc=example,dc=org"
+    echo "Password: admin"
+}
+
+stopcontainer() {
+    docker stop ldap-server
+    docker stop phpldapadmin-service
+    docker rm ldap-server
+    docker rm phpldapadmin-service
+}
+
+case "$1" in 
+    start)   startcontainer ;;
+    stop)    stopcontainer ;;
+    restart) stopcontainer; startcontainer ;;
+    *) echo "usage: $0 start|stop|restart" >&2
+       exit 1
+       ;;
+esac
